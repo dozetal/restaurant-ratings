10b10d2f0da71038f3e92a5b303f365c0c60f5a5
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-10 17:36:24 -0700

    Restaurant Ratings - code is good

--- ratings.py
+++ ratings.py
@@ -2,44 +2,45 @@
 
 # put your code here
 
-"""
-
-Sort and return restaurant ratings
-
-"""
-
 def read_ratings(my_file):
-# Sort and return restaurant ratings
+
+    """
+
+    Sort and return restaurant ratings
+
+    """
 
     ratings_file = open(my_file)
-    text = text_file.read()
-    words = text.split(" ")
-    for word in words:
-        word = word.lower()
-        word = word.strip(string.whitespace)
-        word = word.rstrip(string.punctuation)
-    c = collections.Counter(words)
-    for word, count in c.items():
-        print word, count
-    # word_count = {}
-    # for line in text:
-    #     line = line.rstrip()
-    #     words = line.split(" ")
-    #     for word in words:
-    #         word = word.lower()
-    #         word = word.strip(string.punctuation)
-    #         word_count[word] = word_count.get(word, 0) + 1
-    # for word, count in word_count.items():
-    #     print word, count
-    text_file.close()
+
+    # create dictionary
+    ratings = {}
+
+    # In this loop, split text file into list of Restaurant + Rating
+    for line in ratings_file:
+        line = line.rstrip().split(":")
+        ratings[line[0]] = line[1]
+    
+    # Creating a list of tuples with restaurant name + rating
+    r_items = ratings.items()
+
+    # Sort the list of restaurant name + rating tuples
+    r_items.sort()
+
+
+    # Uncomment the line below to print and confirm the  data
+
+    # print r_items
+
+
+    # Iterate through r_items, print RESTAURANT is rated at RATING
+    # r_item[0] is Restaurant. r_item[1] is Rating.
+
+    for r_item in r_items:
+        print "%s is rated at %s." %(r_item[0], r_item[1])
+
+    # close file
+    ratings_file.close()
 
 
 # Call the function
-read_ratings(my_file = scores.txt)
-  
-
-
-
-
-
-
+read_ratings(my_file = "scores.txt")
